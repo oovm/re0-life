6c6512220d382d30c88092f364c694a3a5aeec44
Aster <galaster@foxmail.com>
2022-12-01 15:02:15 +0800

    Support for more kinds of parentheses

--- projects/re0-webgame/src/main.rs
+++ projects/re0-webgame/src/main.rs
@@ -1,3 +1,36 @@
+use mp4::Result;
+use std::{fs::File, io::BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("tests/video(7).mp4").unwrap();
+    let size = f.metadata().unwrap().len();
+    let reader = BufReader::new(f);
+
+    let mp4 = mp4::Mp4Reader::read_header(reader, size).unwrap();
+
+    // Print boxes.
+    println!("major brand: {}", mp4.ftyp.major_brand);
+    println!("timescale: {}", mp4.moov.mvhd.timescale);
+
+    // Use available methods.
+    println!("size: {}", mp4.size());
+
+    let mut compatible_brands = String::new();
+    for brand in mp4.compatible_brands().iter() {
+        compatible_brands.push_str(&brand.to_string());
+        compatible_brands.push_str(",");
+    }
+    println!("compatible brands: {}", compatible_brands);
+    println!("duration: {:?}", mp4.duration());
+
+    // Track info.
+    for track in mp4.tracks().values() {
+        println!(
+            "track: #{}({}) {} : {}",
+            track.track_id(),
+            track.language(),
+            track.track_type().unwrap(),
+            track.box_type().unwrap(),
+        );
+    }
 }
